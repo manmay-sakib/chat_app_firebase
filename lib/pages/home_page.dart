7004b8c630b3faff61d5a4d501bd5e2cb2ff624b
manmay sakib <monmy.sakib@gmail.com>
2023-09-11 15:18:37 +0600

    chat app interface done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:chat_app_firebase/services/auth/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'chat_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +14,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // instance of auth
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   // sign user Out
   void signOut() {
     // get the auth service
@@ -32,6 +39,56 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      body: _buildUserList(),
     );
+  }
+
+  // build a list of users except for the current logged in user
+
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("error");
+        }
+        if (snapshot.connectionState == ConnectionState) {
+          return const Text('loading...');
+        }
+        return ListView(
+          children: snapshot.data!.docs
+              .map<Widget>((doc) => _buildUserListItem(doc))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  // build individual user list items
+  Widget _buildUserListItem(DocumentSnapshot document) {
+    // ai line ta bujhi nai
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+    // display all users expect current user
+    if (_auth.currentUser!.email != data['email']) {
+      return ListTile(
+        title: Text(data['email']),
+        onTap: () {
+          // pass the clicked user's UID to the chat page
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ChatPage(
+                receiverUserEmail: data['email'],
+                receiverUserID: data['uid'],
+              ),
+            ),
+          );
+        },
+      );
+    } else {
+      // return empty container
+      return Container();
+    }
   }
 }
--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,10 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class AuthService extends ChangeNotifier {
   // instance of auth
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // instance of the firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // sign user in
   Future<UserCredential> signInWithEmailandPassword(
@@ -34,6 +37,19 @@ class AuthService extends ChangeNotifier {
         email: email,
         password: password,
       );
+
+      // add a new document for the user in users collection if it doesn't already exists
+      _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      }, SetOptions(merge: true));
+
+      // after creating the user, create a new document for the user in the users collection
+      _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      });
+
       return userCredential;
     }
     // catch any errors
